20c9adcb9ce2faa869281f2e2a4eed3720c64d46
Connor Corcoran <connorcorc@gmail.com>
2023-01-02 16:20:49 -0700

    fix cypress

--- cypress/e2e/trick.cy.js
+++ cypress/e2e/trick.cy.js
@@ -1,9 +1,9 @@
 describe('trick', () => {
   beforeEach(() => {
-    // cy.intercept({
-    //   method: 'GET',
-    //   url: 'http://localhost:3001/api/v1/tricks'
-    // }, { fixture: 'tricks' })
+    cy.intercept('http://localhost:3001/api/v1/tricks', {
+      method: 'GET',
+      fixture: '../fixtures/tricks.json'
+    })
     cy.visit('http://localhost:3000/')
 
   })
@@ -14,8 +14,6 @@ describe('trick', () => {
 
   it('should display tricks', () => {
     cy.get('.trickContainer > :nth-child(1)')
-    cy.get('.trickContainer > :nth-child(2)')
-    cy.get('.trickContainer > :nth-child(3)')
   })
 
   it('should have a trick name, obstacle, stance', () => {
@@ -26,13 +24,16 @@ describe('trick', () => {
   })
 
   it('should add a trick', () => {
-    cy.get('input').type('BananaFlip')
+    cy.get('[placeholder="Which Trick You Wanna Do?!"]').type('BananaFlip')
     cy.get('[name="stance"]').select('Switch')
     cy.get('[name="obstacle"]').select('Ledge')
-    cy.get('button').click()
-    cy.get('.trickContainer > :nth-child(4)')
-    cy.get(':nth-child(4) > h2').contains('BananaFlip')
+    cy.get('button.option').click()
+    cy.get('.trickContainer > :nth-child(2)')
+    cy.get(':nth-child(2) > h2').contains('BananaFlip')
+  })
 
+  it('should delete trick', () => {
+    cy.get('.trickContainer > :nth-child(1)').click()
   })
 
 
